9e8157510171af43d6faa223f3f3ea00b9fa00cd
donagirok <dagiro@students.kennesaw.edu>
2024-12-06 01:38:24 -0500

    from branch to main

--- test_don.py
+++ test_don.py
@@ -5,3 +5,10 @@ from don import Don
 
 new_don = Don()
 update_don = Don()
+
+try:
+    new_don.load_data('data.csv')
+    update_don.load_data('data.csv')
+except FileNotFoundError:
+    new_don.load_data('tests/data.csv')
+    update_don.load_data('tests/data.csv')
